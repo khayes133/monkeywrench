20c8c8abfd1aa2c0e347e2a9016a52c91a0f77e0
MattB <104602457+mbencomo2@users.noreply.github.com>
2023-06-23 22:46:53 -0700

    Use expressMiddleware from @Apollo/server/express4
    Signed-off-by: MattB <104602457+mbencomo2@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
+const { json } = require("body-parser");
 const { typeDefs, resolvers } = require("./graphql/schema");
 const app = express();
 
 async function startServer() {
-  const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({
+      typeDefs,
+      resolvers
+    }),
+    introspection: true
+  });
+
+  // Start the server
   await server.start();
 
-  const graphqlPath = "/api/graphql";
-  server.applyMiddleware({ app, path: graphqlPath });
+  // Set the path for api calls
+  app.use("/graphql", json(), expressMiddleware(server));
 
   const port = 4000;
   app.listen(port, () => {
-    console.log(`🚀 Server running on http://localhost:${port}${graphqlPath}`);
+    console.log(`🚀 Server running on http://localhost:${port}/graphql`);
   });
 }
 
